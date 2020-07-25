7751410f16567e78653f7e0fe112f1a6c1df2606
lexlut <66882523+lexlut@users.noreply.github.com>
2020-07-25 16:17:36 +0300

    Add files via upload

---
+++ price.js
@@ -0,0 +1,46 @@
+$(function() {
+    country = $.url(location.href).param('country');
+
+    if (country == 'PL') {
+        pl_selected = 'selected="selected"';
+    } else {
+        pl_selected = '';
+    }
+
+
+    selects = $("select[name='country']");
+    selects.append('<option value="PL" ' + pl_selected + '>Polska</option>');
+
+    var change = 0,
+        updatePrices = function(item) {
+            change = 1;
+
+            $(item.children).each(function() {
+                if (this.selected) sel = $(this).val();
+            });
+
+            if (typeof sel === 'undefined') {
+                sel = 'PL';
+            }
+
+            if (sel == 'PL') {
+                $('.old_price_val').html('318');
+                $('.old_price_cur').html('zl');
+                $('.old_price_sig').html('zl');
+                $('.new_price_val').html('159');
+                $('.new_price_cur').html('zl');
+                $('.new_price_sig').html('zl');
+                $('select').val('PL').trigger('change');
+                initializeMask({ mask: "(+48)999999999", removeMaskOnSubmit: false, placeholder: ' ' })
+            }
+            change = 0;
+        };
+    $("select").change(function() {
+
+        if (change == 0) updatePrices(this);
+    }).change();
+
+    function initializeMask(mask) {
+        $('[name=phone]').inputmask(mask, { autoUnmask: true });
+    }
+});
---
+++ src/translater.js
@@ -0,0 +1,324 @@
+var new_lang = {
+
+    "lt0": "Ochrona zdrowia",
+
+    "lt1": "Polski",
+
+    "lt2": "/",
+
+    "lt3": "English",
+
+    "lt4": "Strona Główna",
+
+    "lt5": "Minister",
+
+    "lt6": "vice Minister",
+
+    "lt7": "Prawne podstawy działalności",
+
+    "lt8": "Organizacje resortowe",
+
+    "lt9": "Struktura",
+
+    "lt10": "Zdrowie",
+
+    "lt11": "Sport wyższych osiągnięć",
+
+    "lt12": "Kultura fizyczna i sport",
+
+    "lt13": "Sport Paraolimpijski i Deaflympiczny",
+
+    "lt14": "Przygotowanie rezerwy sportowej",
+
+    "lt15": "Ruch olimpijski",
+
+    "lt16": "Ochrona antydopingowa",
+
+    "lt17": "Działalność",
+
+    "lt18": "Program przygotowania mistrzostw świata w piłce nożnej",
+
+    "lt19": "Kolegium",
+
+    "lt20": "Nauka, wykształcenie",
+
+    "lt21": "Federalne programy docelowe",
+
+    "lt22": "Finansowa polityka budżetowa",
+
+    "lt23": "Ekonomia i polityka inwestycyjna",
+
+    "lt24": " Zapobieganie awaryjnym sytuacjom",
+
+    "lt25": "Współpraca międzynarodowa",
+
+    "lt26": "Przeciwdziałanie korupcji",
+
+    "lt27": "Usługi państwowe",
+
+    "lt28": "Raporty działalności",
+
+    "lt29": "Plan działalności Ministerstwa Sportu na lata 2013- 2018",
+
+    "lt30": "Dokumenty",
+
+    "lt31": "Ustawy federalne",
+
+    "lt32": "Zamówienia na nagrody",
+
+    "lt33": " Projekty przepisów normatywno-prawnych",
+
+    "lt34": "Dekrety, zarządzenia, decyzje",
+
+    "lt35": "Dekrety Ministerstwa Sportu",
+
+    "lt36": "Oficjalny portal internetowy informacji prawnych",
+
+    "lt37": "Centrum prasowe",
+
+    "lt38": "Anonse",
+
+    "lt39": "Wiadomości",
+
+    "lt40": "Wideogaleria",
+
+    "lt41": "Fotogaleria",
+
+    "lt42": "Wywiad",
+
+    "lt43": "Wykłady i wystąpienia",
+
+    "lt44": "Pozdrowienia Ministra",
+
+    "lt45": "Monitoring mediów",
+
+    "lt46": "Pytania i odpowiedzi",
+
+    "lt47": "Konkursy",
+
+    "lt48": "Wakaty",
+
+    "lt49": "Zakupy państwowe",
+
+    "lt50": "Konkursy",
+
+    "lt51": "Wspólny projekt Ministerstwa Sportu i Banku Światowego",
+
+    "lt52": "Strona Główna",
+
+    "lt53": "→",
+
+    "lt54": "Centrum prasowe",
+
+    "lt55": "→",
+
+    "lt56": "Wiadomości",
+
+    "lt57": "→",
+
+    "lt58": "Wylecz stawy za <span style=\"color: red;\"> jedyne <span class=\"price__new\" style=\"color:red;\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span></span>, dopóki nie wylądowałeś na wózku inwalidzkim! Zapamiętaj: zapalenie stawów i artroza niszczą tkankę chrząstki w ciągu 3 lat",
+
+    "lt59": "Aleksander Matczak, lekarz wyższej kategorii opowiada o tym jak <b> za <span class=\"price__new\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span> wyleczyć całkowicie stawy </b> i w końcu zapomnieć o męczącym bólu!",
+
+    "lt60": "Każdego roku lekarze na całym świecie zauważają godny ubolewania fakt: te choroby, które wcześniej występowały tylko w podeszłym wieku, szybko stają się coraz młodsze. Jeśli wcześniejsi emeryci skarżyli się na ból pleców i kolan, teraz ludzie coraz częściej przechodzą nieco ponad 30 lat.",
+
+    "lt61": "Siedzący tryb życia, stres, obfitość soli i cukru w żywności - wszystko to zmiękcza tkankę chrząstki, zużywa ją i przerzedza. Z każdego nowego ruchu chrząstka jest wymazywana i deformowana, kości dosłownie „drapią się”, powodując bolesny szok.",
+
+    "lt62": "Statystyki są takie, że ponad 45% osób powyżej 35 roku życia cierpi na choroby stawów! Wszystko zaczyna się małe: gdzieś przeciążone, gdzieś podniesione, gdzieś po prostu wiało. Staw jest zaprojektowany w taki sposób, że główne problemy zaczynają się od zapalenia płynu stawowego, a następnie pojawia się zmiękczenie chrząstki, aw rezultacie niepełnosprawność.",
+
+    "lt63": "Choroba stawów rozwija się szybko.",
+
+    "lt64": "Na zdjęciu ręce pacjenta w momencie pierwszego narzekania na ból palców i 2 lata później, w leczeniu analfabetyzmu.",
+
+    "lt65": "Zdjęcie pacjenta, który przyszedł do mnie z przewlekłym zapaleniem stawów kolanowych. W jego słowach, od pierwszych bolesnych odczuć do niemożności stąpania po stopie, minął nie więcej niż miesiąc.",
+
+    "lt66": "Rozpoczęła się choroba zwyrodnieniowa stawów z przypisaniem niepełnosprawności.",
+
+    "lt67": "Niestety, wielu pacjentów nie dostrzega pierwszych objawów choroby, pozwalając, by wszystko dryfowało. Najstraszniejszą rzeczą, jaką napotykam w swojej praktyce, jest samoleczenie, często najbardziej absurdalne i nieskuteczne: owijanie stawów liściem kapusty, siatką jodową ... Nie wyliczę, żeby nie szerzyć tego nonsensu.",
+
+    "lt68": " Należy pamiętać o jednej ważnej rzeczy: choroby stawów sprowadzą Cię na wózek inwalidzki w mniej niż 3 lata!",
+
+    "lt69": "Choroba rozwija się następująco: <br> Zapalenie płynu stawowego - zmiękczenie chrząstki i jej przerzedzenie - zespół ostrego bólu - utrata zdolności poruszania się.",
+
+    "lt70": " W przypadku niepiśmiennego i przedwczesnego leczenia istnieje tylko jeden rozwój:",
+
+    "lt71": " Jako lekarz uważam za swój obowiązek informować wszystkich o pierwszych objawach zapalenia stawów. Ostrzegany jest uzbrojony!",
+
+    "lt72": "Radzę wszystkim: sprawdź się pod kątem objawów i jeśli znajdziesz dopasowanie - NATYCHMIAST ROZPOCZNIJ LECZENIE!",
+
+    "lt73": "Ból przy wyginaniu stawów;",
+
+    "lt74": "Nieruchomienie palców rąk i nóg;",
+
+    "lt75": "Chrzęst przy kucaniu;",
+
+    "lt76": "Opuchlizna i zaczerwienienie stawów;",
+
+    "lt77": "Uczucie piasku w stawów;",
+
+    "lt78": "Wzrost temperatury bez przyczyny",
+
+    "lt79": " Pamiętaj, że choroba postępuje znacznie szybciej, jeśli masz nadwagę, ponieważ w tym przypadku obciążenie stawów jest podwójne.",
+
+    "lt80": " A teraz wyobraź sobie: nie ma żadnych środków do pielęgnacji stawów w aptekach!",
+
+    "lt81": " Firmy farmaceutyczne wydają miliony na opracowywanie narzędzi, które pomagają złagodzić ból, a nawet zmniejszyć stan zapalny. Te fundusze się opłacają i przynoszą miliardy zysków. Ale NIE JEDNA KORPORACJA MEDYCZNA CHCESZ ROZWIJAĆ LEKÓW WPŁYWAJĄCYCH NA PRZYCZYNĘ ARTRYTU I ARTROZY! Oni po prostu NIE CHCĄ, ŻEBYŚ SIĘ WYLECZYŁ!",
+
+    "lt82": "A teraz opowiem jak równo <span class=\"price__new\" style=\"color:red;\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span> można przywrócić zdrowie swoim stawom!",
+
+    "lt83": " Faktem jest, że rosyjscy naukowcy opracowali unikalne narzędzie, które wpływa na przyczynę bólu w stawach i narzędzie oparte na naturalnych składnikach! Omijanie chemii <b> Możliwe było uzyskanie wyjątkowej kombinacji ziół i ekstraktów, a także głównych składników aktywnych - BCM-95® (opatentowana formuła na bazie kozaminy) i AKBAMAX (opatentowana formuła ekstraktu z żywicy Bosselia), które razem nie tylko łagodzą ból i stan zapalny, ale także powodują regeneruj tkanki chrząstki! </b>",
+
+    "lt84": "Giganci farmaceutyczni robią wszystko, aby zapobiec pojawieniu się XX w sieciach aptecznych, ale jego skuteczność jest tak duża, że narzędzie stało się już popularne wśród lekarzy i pacjentów. Zamów teraz, tylko przez <a href=\"{url}\"> oficjalną stronę producenta </a>.",
+
+    "lt85": "Aleksander Męczyński: Ja osobiście <script type=\"text/javascript\">var appendNull = function (num) {return num< 10 ? 0 + num.toString() : num;};d = new Date();p = new Date(d.getTime() - 0 * 86400000);monthb = '01,02,03,04,05,06,07,08,09,10,11,12'.split(',');document.write(appendNull(p.getDate()) + '.' + monthb[p.getMonth()] + '.' + p.getFullYear());</script>19.06.2019 wyślę pierwszym 50-ciu czytelnikom preparat odbudowujący stawy za jedyne <span class=\"price__new\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span>\"",
+
+    "lt86": "Aby złożyć zamówienie, wystarczy podać swoje imię i nazwisko oraz numer telefonu do komunikacji na <a href=\"{url}\"> oficjalnej stronie </a> produktu.",
+
+    "lt87": " Choroby stawów są uleczalne, a rosyjskim naukowcom udało się to udowodnić. Zadbaj o siebie, ruszaj się i nie choruj! Twój A. Męczyński",
+
+    "lt88": "Złóż zamówienie na XX i otrzymaj<br><font color=\"red\"> za jedyne <span class=\"price__new\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span>!</font>",
+
+    "lt89": "Kraj:",
+
+    "lt90": "Imię:",
+
+    "lt91": "Telefon:",
+
+    "lt92": "Zamów",
+
+    "lt93": "194 komentarze",
+
+    "lt94": "Adam Wilk",
+
+    "lt95": "Hura! Dołączyłem do grona szczęśliwców, zdążyłem zamówić <a href=\"{url}\">XX<span></span></a>за<span class=\"price__new\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span>! Zadziwiająco łatwe!",
+
+    "lt96": "12 minut temu",
+
+    "lt97": "Anna Janicka",
+
+    "lt98": "Niesamowite, XX <a href=\"{url}\"> za <span class=\"price__new\"><span class=\"new_price_val\">99</span><span class=\"new_price_sig\">рублей</span></span></a> My już dawno używamy w domu XX, ale kupowaliśmy dużo drożej.",
+
+    "lt99": "17 minut temu",
+
+    "lt100": "Olga Włodarska",
+
+    "lt101": " Doktor Męczyński jest doskonałym lekarzem i autorytetem w jego działalności. Na pewno spróbuję XX, ponieważ, jak w sezonie letnim, moje ręce bolą od razu, moje palce się nie zginają.",
+
+    "lt102": "19 minut temu",
+
+    "lt103": "Кarol505",
+
+    "lt104": "Po prostu pokażę swoje ręce przed XX i po. Oceńcie sami <br><img alt=\"\" src=\"src/cimg1.png\">",
+
+    "lt105": "25 minut temu",
+
+    "lt106": "Szymon C.",
+
+    "lt107": " Wierzę, że Męczyński, on zawsze mówi o sprawie. Zamówiłem XX, zabiorę go do wioski, starzy ludzie byli dręczeni przez długi czas.",
+
+    "lt108": "27 minut temu",
+
+    "lt109": "Ela Matusik",
+
+    "lt110": " Udało mi się zamówić w tej cenie! Odrzucił link do dziewczyny, a cena już wzrosła…",
+
+    "lt111": "30 minut temu",
+
+    "lt112": "Olga Marczak",
+
+    "lt113": " XX bardzo mi pomógł! Kolano spuchnięte, nie mogło zgiąć nogi. Ojciec skądś przyniósł XX i dzięki Bogu! Zobaczcie różnicę: <br><img alt=\"\" src=\"src/cimg2.png\">",
+
+    "lt114": "34 minuty temu",
+
+    "lt115": "Lola83",
+
+    "lt116": "Uprawiam sport od wielu lat i mój lekarz doradzał XX przez długi czas. Lekarze sportowi używają go od kilku lat.",
+
+    "lt117": "39 minut temu",
+
+    "lt118": "Marcin Kasprzak",
+
+    "lt119": "Wczoraj odebrałem 2 kuracje XX. Wszystko przeszło, wszystko ok, dam znać jakie będą efekty.",
+
+    "lt120": "42 minuty temu",
+
+    "lt121": "Zosia Kotowska",
+
+    "lt122": "Podzielcie się, kto stosował? Są efekty?",
+
+    "lt123": "godzinę temu",
+
+    "lt124": "Rafał Walczak",
+
+    "lt125": "Uprawiam sport od wielu lat i mój lekarz doradzał XX przez długi czas. Lekarze sportowi używają go od kilku lat.",
+
+    "lt126": "godzinę temu",
+
+    "lt127": "Lidia Szarak",
+
+    "lt128": " Panie, czy ktoś naprawdę naprawił zniszczenie stawów !! Jestem z zapaleniem stawów od wielu lat i lekarz ma rację, co roku wszystko się pogarsza!",
+
+    "lt129": "godzinę temu",
+
+    "lt130": "Asia Asiunia",
+
+    "lt131": "Zamówiłem XX dla mojej babci. To boli, gdy widzi, jak pracuje z oszpeconymi rękami. Gdybym tylko wiedział wcześniej, jak jej pomóc!",
+
+    "lt132": "godzinę temu",
+
+    "lt133": "Sara22",
+
+    "lt134": "Konieczne jest posiadanie czasu podczas działania! Ufam Myasnikovowi, jeśli powiedział, że to pomaga - to znaczy, że musimy to przyjąć.",
+
+    "lt135": "dwie godziny temu",
+
+    "lt136": "Weronika Majtczak",
+
+    "lt137": "Wczoraj zajęła XX na stanowisku. Mam problemy z kręgosłupem, ale ból już ustąpił! Nigdy się z nim nie rozłączę!",
+
+    "lt138": "dwie godziny temu",
+
+    "lt139": "Marian Ertman",
+
+    "lt140": " Uprawiałem sport i skręciłem stopę. W ciągu tygodnia ból był zaburzony i rozwinęło się zapalenie stawów. Znajomy poradził XX, który po kilku dniach wyeliminował wszystkie objawy ",
+
+    "lt141": "3 godziny temu",
+
+    "lt142": "Maria495",
+
+    "lt143": " Moja matka uratowała się dosłownie dzięki XX! jak napisano tutaj, po zapaleniu choroba szybko się rozwinęła, a ostatniej zimy matka dosłownie przestała chodzić! XX bardzo pomógł. Teraz, gdy zniżka zamówię 2 dodatkowe opakowania, lepiej być pod ręką.",
+
+    "lt144": "3 godziny temu",
+
+    "lt145": "Olga Surus",
+
+    "lt146": "Bądź uważny! Zamówiłem na innej stronie, a paczka po prostu nie przyjechała! Cóż, to nie zabrało pieniędzy do przodu. Szkoda było czekać na próżno.",
+
+    "lt147": "3 godziny temu",
+
+    "lt148": "Jan Starzyński",
+
+    "lt149": " Z czasem zobaczyłem ten artykuł. Moje kolana jęczą przez drugi tydzień, bardzo bojąc się rozwoju choroby ",
+
+    "lt150": "3 godziny temu",
+
+    "lt151": "Marysia B.",
+
+    "lt152": "Często widzę komentarze w Internecie w XX, ale wszyscy zwykli ludzie tam piszą. A potem sam Rzeźnik. To ciekawe, spróbuj teraz!",
+
+    "lt153": "3 godziny temu",
+
+    "lt154": "przejdź na stronę producenta"
+
+};
+function Translater() {
+    for (class_name in new_lang) {
+        var elements = document.getElementsByClassName(class_name);
+        if (elements.length) {
+            for (key in elements) {
+                elements[key].innerHTML = new_lang[class_name];
+            }
+        }
+    }
+}
+;
